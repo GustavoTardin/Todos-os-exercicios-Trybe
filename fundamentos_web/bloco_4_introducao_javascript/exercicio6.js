4805679e271637ec16ca97e38aa7d55395e8ee51
GustavoTardin <gustavofilipetardin@outlook.com>
2022-06-21 20:02:59 -0300

    Realiza exercicio sobre Switch/case

---
+++ fundamentos_web/bloco_4_introducao_javascript/exercicio6.js
@@ -0,0 +1,20 @@
+let pessoaCandidata = "reprovada";
+switch(pessoaCandidata){
+    case "aprovada":
+    console.log("Parabéns, você é muito inteligente")
+    break;
+
+    case "lista":
+    console.log("Caso haja desistência, você conseguirá o que quer")
+    break;
+
+    case "reprovada":
+        console.log("Você é muito burro")
+        break;
+
+        default:
+            console.log("Valor não encontrado")
+}
+
+
+
