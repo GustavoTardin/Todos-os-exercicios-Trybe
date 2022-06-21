4192b2a9caafffab663047d35653ccdb8b27326f
GustavoTardin <gustavofilipetardin@outlook.com>
2022-06-21 16:15:02 -0300

    Realiza 3 exercicios sobre comandos basicos de javascript

---
+++ fundamentos_web/bloco_4_introducao_javascript/exercicio1.js
@@ -0,0 +1,8 @@
+const myName = "Gustavo";
+const birthCity = "Nova Friburgo";
+let birthYear = 2003;
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ fundamentos_web/bloco_4_introducao_javascript/exercicio2.js
@@ -0,0 +1,15 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+const base = 5;
+const height = 8;
+const area = base * height;
+const perimeter = base * 2 + height * 2;
+console.log(area);
+console.log(perimeter);
+
