2870e39777e6b754c38b8135efed7363b17c7d9b
GustavoTardin <gustavofilipetardin@outlook.com>
2022-07-15 15:43:38 -0300

    Adiciona arquivos

---
+++ fundamentos_web/bloco_7_JavaScript_ES6/dia1/exercicios/script.js
@@ -0,0 +1,36 @@
+/* const testingScope = (escopo) =>{
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} 
+      ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+   
+  }
+
+  testingScope(true);*/
+
+/*const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const crescente = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens;
+}
+
+console.log(`Os números ${crescente()} se encontram ordenados de forma crescente!`)*/
+
+
+//console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+
+const fatorial = (num) => {
+  
+}
